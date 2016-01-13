170bf508c000b38630173bf92cbc48d69f107215
kiwenlau <kiwenlau@126.com>
2016-01-13 16:08:59 +0900

    share 1 directory successfully

--- test.sh
+++ test.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo -e "\nbuild docker image"
 sudo docker build -t kiwenlau/nfs-docker .
 
 sudo docker rm -f nfs-server nfs-client &> /dev/null
 
-sudo docker run --privileged -d -it --name nfs-server kiwenlau/nfs-docker start-nfs-server.sh /opt/test
+echo -e "\n\nstart nfs server container"
+sudo docker run --privileged -d -it --name nfs-server kiwenlau/nfs-docker start-nfs-server.sh /opt > /dev/null
 
 NFS_SERVER_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" nfs-server)
 
-sudo docker run --privileged -d --name nfs-client --link nfs-server:nfs-server -e "NFS_SERVER_IP=$NFS_SERVER_IP" -it -v /opt kiwenlau/nfs-docker start-nfs-client.sh /opt/test:/opt/test
+echo -e "\nstart nfs client container"
+sudo docker run --privileged -d --name nfs-client --link nfs-server:nfs-server -e "NFS_SERVER_IP=$NFS_SERVER_IP" -it kiwenlau/nfs-docker start-nfs-client.sh /opt:/opt > /dev/null
+
+echo ""
+
