5e5405cd5154226c3ca82cba74c2f6d50282e981
kiwenlau <kiwenlau@126.com>
2016-01-13 16:14:34 +0900

    share /data failed

--- test.sh
+++ test.sh
@@ -6,12 +6,12 @@ sudo docker build -t kiwenlau/nfs-docker .
 sudo docker rm -f nfs-server nfs-client &> /dev/null
 
 echo -e "\n\nstart nfs server container"
-sudo docker run --privileged -d -it --name nfs-server kiwenlau/nfs-docker start-nfs-server.sh /opt > /dev/null
+sudo docker run --privileged -d -it --name nfs-server kiwenlau/nfs-docker start-nfs-server.sh /opt /data > /dev/null
 
 NFS_SERVER_IP=$(docker inspect --format="{{.NetworkSettings.IPAddress}}" nfs-server)
 
 echo -e "\nstart nfs client container"
-sudo docker run --privileged -d --name nfs-client --link nfs-server:nfs-server -e "NFS_SERVER_IP=$NFS_SERVER_IP" -it kiwenlau/nfs-docker start-nfs-client.sh /opt:/opt > /dev/null
+sudo docker run --privileged -d --name nfs-client --link nfs-server:nfs-server -e "NFS_SERVER_IP=$NFS_SERVER_IP" -it kiwenlau/nfs-docker start-nfs-client.sh /opt:/opt /data:/data > /dev/null
 
 echo ""
 
